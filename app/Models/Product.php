1316fd11e26a5b57604f206c38a4e2c156201f45
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2023-08-03 16:27:30 +0200

    Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use TCG\Voyager\Traits\Resizable;
+use TCG\Voyager\Traits\Translatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
+    use Translatable;
+    use Resizable;
+
+    protected $translatable = ['title', 'body', 'slug', 'categories', 'tags', 'seo_title', 'seo_description'];
+
+    protected $guarded = [];
 
     public function categories()
     {
